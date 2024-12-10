c7513a3284f003b8233e4055177b83b402f8e883
Lam Huynh <lam.huynh@zoi.tech>
2024-12-10 19:35:44 +0700

    feat: configure Storybook and VS Code

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,44 @@ import type { Preview } from '@storybook/react';
 import '@festo-ui/web-essentials/dist/css/festo-web-essentials.min.css';
 import '@festo-ui/react/index.css';
 
+const customViewports = {
+  xl: {
+    name: '1920',
+    styles: {
+      width: '1920px',
+      height: '1080px',
+    },
+  },
+  l: {
+    name: '1440',
+    styles: {
+      width: '1440px',
+      height: '800px',
+    },
+  },
+  m: {
+    name: '1024',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    },
+  },
+  s: {
+    name: '768',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  xs: {
+    name: '375',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +47,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: customViewports,
     },
   },
 };
