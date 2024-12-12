c2e8be4b9148d58b461e64aa638c751d7b35aa5a
Lam Huynh <lam.huynh@zoi.tech>
2024-12-12 19:21:23 +0700

    feat: add Tailwind CSS

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,7 +5,7 @@ import { pluginSass } from '@rsbuild/plugin-sass';
 export default defineConfig({
   source: {
     entry: {
-      index: './src/rtk-query/index.tsx',
+      index: './src/i18n/index.tsx',
     },
   },
   plugins: [pluginReact(), pluginSass()],
