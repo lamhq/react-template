8987fcde8bf06fff4bffd8b7281ad178ed289f01
Lam <daibanglam@gmail.com>
2025-07-01 09:40:03 +0700

    feat: add Storybook and enable msw

---
+++ .storybook/msw.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+
+  http.post('api/auth/access-tokens', () => {
+    return HttpResponse.json({
+      user: {
+        id: '123',
+        email: 'test@test.com',
+      },
+    });
+  }),
+
+];
---
+++ .storybook/preview.ts
@@ -0,0 +1,33 @@
+import type { Preview } from '@storybook/react-vite'
+import { initialize, mswLoader } from 'msw-storybook-addon'
+import { handlers } from './msw'
+
+// Initialize MSW
+initialize()
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    },
+    
+    // Add the MSW handlers to the preview
+    msw: {
+      handlers,
+    },    
+  },
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader],
+};
+
+export default preview;
