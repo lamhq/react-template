1227c492a9003cccc4b355da4713abcb622fe3dc
Lam <daibanglam@gmail.com>
2025-07-01 11:57:01 +0700

    feat: add React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
 import App from './App.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssVarsProvider>
       <CssBaseline />
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </CssVarsProvider>
   </StrictMode>,
