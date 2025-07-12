bf6dc56a1e63365b5a53c4228b03edaa33bea7d0
Lam <daibanglam@gmail.com>
2025-07-12 16:48:20 +0700

    feat: implement optimistic update for DeleteTodoBtnContainer

--- src/todos/organisms/DeleteTodoBtn/DeleteTodoBtnContainer.tsx
+++ src/todos/organisms/DeleteTodoBtn/DeleteTodoBtnContainer.tsx
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useAtomValue } from 'jotai';
 import { useConfirm } from '../../../confirm';
 import { useNotification } from '../../../notification';
 import { deleteTodo, type Todo } from '../../api';
+import { todoListPageAtom } from '../../atoms';
 import { TODO_QUERY_KEY } from '../../constants';
 import DeleteTodoBtn from './DeleteTodoBtn';
 
@@ -13,17 +15,42 @@ export default function DeleteTodoBtnContainer({
   todo,
 }: DeleteTodoBtnContainerProps) {
   const queryClient = useQueryClient();
-  const { showSuccess, showError } = useNotification();
+  const page = useAtomValue(todoListPageAtom);
+  const { showError } = useNotification();
   const confirm = useConfirm();
   const { mutate, isPending } = useMutation({
     mutationFn: () => deleteTodo(todo.id),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [TODO_QUERY_KEY] });
-      showSuccess('Todo deleted!');
+    onMutate: async () => {
+      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: [TODO_QUERY_KEY, page] });
+
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData([TODO_QUERY_KEY, page]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(
+        [TODO_QUERY_KEY, page],
+        (old: [Todo[], number] | undefined) => {
+          if (!old) return old;
+          const [todos, total] = old;
+          return [todos.filter((t) => t.id !== todo.id), total - 1];
+        },
+      );
+
+      // Return a context object with the snapshotted value
+      return { previousTodos };
     },
-    onError: (err: Error) => {
+    onError: (err: Error, __, context) => {
+      // If the mutation fails, use the context returned from onMutate to roll back
+      if (context?.previousTodos) {
+        queryClient.setQueryData([TODO_QUERY_KEY, page], context.previousTodos);
+      }
       showError(err.message);
     },
+    // onSettled: () => {
+    //   // Always refetch after error or success to ensure cache consistency
+    //   queryClient.invalidateQueries({ queryKey: [TODO_QUERY_KEY, page] });
+    // },
   });
 
   const handleDelete = async () => {
